f5acb49149ae4c5c24692a856195144402a05def
s1f102200558 <s1f102200558@iniad.org>
2023-07-20 12:20:41 +0900

    格言を表示

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
+from django.http.response import JsonResponse
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
+import os
+import openai
+import json
 
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_base = "https://api.openai.iniad.org/api/v1"
 
 # Create your views here.
 def index(request):
@@ -71,3 +77,42 @@ def close(request, task_id):
     task.completed = True
     task.save()
     return redirect(index)
+
+
+def parse(response: str):
+    json_string = response.split("```json")[1].strip().split("```")[0].strip()
+    data = json.loads(json_string)
+    text = data.get("saying")
+    return text
+
+
+def saying(request):
+    if request.method != "GET":
+        return JsonResponse({"error": "GET method only"}, status=400)
+
+    if openai.api_key is None:
+        return JsonResponse({"saying": "格言かもしれない"}, status=200)
+
+    system_message = """The output should be a markdown code snippet formatted in the following schema:
+
+```json
+{
+    saying: str, // "saying in Japanese"
+}
+```"""
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "system",
+            "content": system_message,
+        }, {
+            "role": "user",
+            "content": "格言を生成してください。"
+        }],
+    )
+
+    response = completion["choices"][0]["message"]["content"]
+    message = parse(response)
+    
+    return JsonResponse({"saying": message}, status=200)
